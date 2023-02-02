d8f8de3d1e5c44a7f31842a2487751e6ecce94a8
“ilhanu” <ilhanunlu@gmail.com>
2023-02-02 09:57:50 +0100

    fix edit path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/fair-squares/fs-docs',
+            'https://github.com/fair-squares/fs-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
