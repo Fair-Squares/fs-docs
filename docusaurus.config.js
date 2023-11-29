386cf9f6527747deb008faa138c5e1c225810dca
“ilhanu” <ilhanunlu@gmail.com>
2023-11-29 21:54:59 +0100

    update to docusaurus v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -143,9 +144,9 @@ const config = {
         ],
         copyright: `Copyright © ${new Date().getFullYear()} Fair Squares, Built with Docusaurus.`,
       },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+      themes: {
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
     }),
 };
